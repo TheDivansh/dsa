b032f80dffae81f6adca93fec0b69425c02aa639
Divansh <Divanshb.007@gmail.com>
2026-09-16 15:54:22 +0530

    Add new print functions and update input/output files

--- 1.cpp
+++ 1.cpp
@@ -216,7 +216,112 @@ void print18(int n){
     }
     
 }
+void print19(int n){
+    int iniS = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j<=n-i; j++)
+        {
+            cout<<"*";
+        }
+        for (int j = 0; j < iniS; j++)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j<= n-i; j++)
+        {
+            cout<<"*";
+        }
+        iniS+=2;
+        cout<<endl;
+        
+    }
+    iniS = 2*n-2;
+    for (int i = 1; i <=n; i++)
+    {
+        for (int j = 1; j<=i; j++)
+        {
+            cout<<"*";
+        }
+        for (int j = 0; j < iniS; j++)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j<=i; j++)
+        {
+            cout<<"*";
+        }
+        iniS-= 2;
+        cout<<endl;
+    }
 
+    
+}
+void print20(int n){
+    int spaces = 2*n-2;
+    for (int i = 1; i <2*n-1; i++)
+    {
+        int stars =i;
+        if (i>n)
+        {
+            stars = 2*n-i;
+        }
+        //stars 
+        for (int j = 1; j <=stars; j++)
+        {
+            cout<<"*";
+        }
+        //Spaces
+        for (int j = 1; j <=spaces; j++)
+        {
+            cout<<" ";
+        }
+        
+        //stars 
+        for (int j = 1; j <=stars; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        if (i<n)
+        {
+            spaces -=2;
+        }
+        else spaces +=2;
+        
+    }
+    
+}
+void print21(int n){
+    for (int i = 0; i <n; i++)
+    {
+        for (int j = 0; j <n; j++)
+        {
+            if (i==0 || j==0 || i==n-1 || j==n-1)
+            {
+                cout<<"*";
+            }
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+void print22(int n){
+    for (int i = 0; i <2*n-1; i++)
+    {
+        for (int j = 0; j <2*n-1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2*n-2)-j;
+            int bottom = (2*n-2)-i;
+            cout<<(n-min(min(top,bottom),min(left,right)));
+        }
+        cout<<endl;
+    }
+    
+}
 int main()
 {
     int t;
@@ -225,6 +330,6 @@ int main()
     {
         int n;
         cin >> n;
-        print18(n);
+        print22(n);
     }
 }
